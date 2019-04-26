d32079bbbf2ecbc1c89236230534f33d06583b0d
Marc Dwyer <marcdwyer1738@gmail.com>
2019-04-26 18:55:26 -0500

    init commit

---
+++ src/components/Main/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './main-styles.scss'
+
+const Homepage = () => {
+    return (
+        <span>hello</span>
+    )
+}
+
+export default Homepage
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Main from './components/Main/main';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={Main} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
